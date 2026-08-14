6ea8dc020180ac149c809dddc366a1533c4b0c3e
arendon7 <102555888+arendon7@users.noreply.github.com>
2026-08-14 11:20:41 -0500

    Wave 38: launch analytics runtime in Full Mac app

--- scripts/build_full_mac_app.sh
+++ scripts/build_full_mac_app.sh
@@ -91,7 +91,7 @@ os.environ.setdefault("BINARIO_FFMPEG", str(resources / "runtime" / "media" / "b
 os.environ.setdefault("BINARIO_FFPROBE", str(resources / "runtime" / "media" / "bin" / "ffprobe"))
 os.environ.setdefault("BINARIO_WHISPER_CLI", str(resources / "runtime" / "transcription" / "bin" / "whisper-cli"))
 os.environ.setdefault("BINARIO_WHISPER_MODEL", str(resources / "runtime" / "transcription" / "models" / "ggml-tiny.bin"))
-from binario_marketing.service_wave37_app import serve
+from binario_marketing.service_wave38_app import serve
 port = int(os.environ.get("BINARIO_PORT", "0"))
 open_browser = os.environ.get("BINARIO_NO_BROWSER") != "1"
 serve("127.0.0.1", port, open_browser=open_browser)
