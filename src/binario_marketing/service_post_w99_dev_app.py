7c81a3e5c746106c0c4bdd64c114753215f1e2f7
arendon7 <102555888+arendon7@users.noreply.github.com>
2026-09-13 16:14:48 -0500

    Advance serve-dev to pilot daily receipt

--- src/binario_marketing/service_post_w99_dev_app.py
+++ src/binario_marketing/service_post_w99_dev_app.py
@@ -42,7 +42,8 @@ from .service_post_w99_pilot_data_safety_app import AppRuntime as _PilotDataSafe
 from .service_post_w99_pilot_session_status_app import AppRuntime as _PilotSessionStatusAppRuntime
 from .service_post_w99_pilot_language_polish_app import AppRuntime as _PilotLanguagePolishAppRuntime
 from .service_post_w99_pilot_recovery_rehearsal_app import AppRuntime as _PilotRecoveryRehearsalAppRuntime
-from .service_post_w99_pilot_launch_gate_app import (
+from .service_post_w99_pilot_launch_gate_app import AppRuntime as _PilotLaunchGateAppRuntime
+from .service_post_w99_pilot_daily_receipt_app import (
     AppRuntime,
     MarketingHandler,
     MarketingHTTPServer,
